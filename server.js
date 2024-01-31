aaee4d1607299478c01e6ea0bc4d82aea12f92c2
ng1n3 <code.with.muyiwa@gmail.com>
2024-01-31 09:34:43 +0100

    fixed bug for cached data

--- server.js
+++ server.js
@@ -2,18 +2,73 @@ const express = require("express");
 const axios = require("axios");
 const redis = require("redis");
 const responseTime = require("response-time");
+const { promisify } = require("util");
+
 
 const app = express();
 
-app.use(responseTime())
+app.use(responseTime());
 
-app.get('/rockets', async (req, res, next) => {
+const client = redis.createClient({
+  legacyMode: true,
+  port: 6379,
+});
+
+const GET_ASYNC = promisify(client.get).bind(client);
+const SET_ASYNC = promisify(client.set).bind(client);
+
+
+
+// client.on("error", (err) => console.log("Error", err));
+client.connect().catch(err => console.log(err))
+
+app.get("/rockets", async (req, res, next) => {
   try {
-    const response = await axios.get('https://api.spacexdata.com/v3/rockets')
-    res.send(response.data)
+    const reply = await GET_ASYNC("rockets");
+    if (reply) {
+      console.log("using cached data");
+      res.send(JSON.parse(reply));
+      return;
+    }
+    const response = await axios.get("https://api.spacexdata.com/v3/rockets");
+    const saveResult = await SET_ASYNC(
+      "rockets",
+      JSON.stringify(response.data),
+      "EX",
+      15
+      );
+      console.log(saveResult, 'PING!');
+    console.log("new data cached", saveResult);
+    res.send(response.data);
   } catch (error) {
-    res.send(error.message)
+    res.send(error);
   }
-})
+});
 
-app.listen(3030, () => console.log("🚀 is on http://localhost:3000"));
+app.get("/rockets/:rocket_id", async (req, res, next) => {
+  try {
+    const { rocket_id } = req.params;
+    const reply = await GET_ASYNC(rocket_id);
+    if (reply) {
+      console.log("using cached data");
+      res.send(JSON.parse(reply));
+      return;
+    }
+    const response = await axios.get(
+      `https://api.spacexdata.com/v3/rockets/${rocket_id}`
+    );
+    const saveResult = await SET_ASYNC(
+      rocket_id,
+      JSON.stringify(response.data),
+      "EX",
+      15
+    );
+    console.log(saveResult, "PING");
+    console.log("new data cached", saveResult);
+    res.send(response.data);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(3030, () => console.log("🚀 is on http://localhost:3030"));
